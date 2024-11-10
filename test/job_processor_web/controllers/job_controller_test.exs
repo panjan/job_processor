4e47ddb3d30d584480bcfa3a6e00b3934a1d452c
Jan Zdráhal <jan.zdrahal@kiwi.com>
2024-11-10 23:30:21 +0100

    Deduplicate code

--- test/job_processor_web/controllers/job_controller_test.exs
+++ test/job_processor_web/controllers/job_controller_test.exs
@@ -1,26 +1,24 @@
 defmodule JobProcessorWeb.JobControllerTest do
   use JobProcessorWeb.ConnCase
 
-  test "POST /api/job", %{conn: conn} do
-    request_file_path = Path.join(__DIR__, "../../support/example_request.json")
-    {:ok, request_content} = File.read(request_file_path)
-    {:ok, request_json} = Jason.decode(request_content)
+  defp read_json_file(file_path) do
+    file_path
+    |> File.read!()
+    |> Jason.decode!()
+  end
 
-    response_file_path = Path.join(__DIR__, "../../support/example_response.json")
-    {:ok, response_content} = File.read(response_file_path)
-    {:ok, expected_response_json} = Jason.decode(response_content)
+  test "POST /api/job", %{conn: conn} do
+    request_json = read_json_file(Path.join(__DIR__, "../../support/example_request.json"))
+    expected_response_json = read_json_file(Path.join(__DIR__, "../../support/example_response.json"))
 
     conn = post(conn, "/api/job", request_json)
     assert json_response(conn, 200) == expected_response_json
   end
 
   test "POST /api/job_script", %{conn: conn} do
-    request_file_path = Path.join(__DIR__, "../../support/example_request.json")
-    {:ok, request_content} = File.read(request_file_path)
-    {:ok, request_json} = Jason.decode(request_content)
+    request_json = read_json_file(Path.join(__DIR__, "../../support/example_request.json"))
+    script = File.read!(Path.join(__DIR__, "../../support/example_bash_script.sh"))
 
-    response_script_file_path = Path.join(__DIR__, "../../support/example_bash_script.sh")
-    {:ok, script} = File.read(response_script_file_path)
     expected_response_json = %{"script" => script}
 
     conn = post(conn, "/api/job_script", request_json)
